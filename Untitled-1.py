98d023e1a0d51e0c8b1cc38881c5a3031db62fc3
Martinbobot <Bobot.Martin@icloud.com>
2025-02-06 14:23:55 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+print("Zadejte vstup")          #vypis toho co chceme 
+text = input()            #deklerace proměnné a její naplnění inputem (vstupem)
+print("Napsali jste: " + text)           #výpis proměnné 
+
